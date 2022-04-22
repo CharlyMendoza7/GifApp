5d0f1727840acbe905b984b016a6624255cf95f1
Carlos <carlosmendoza1719@gmail.com>
2022-04-22 14:32:31 -0500

    my first commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+const GifExpertApp = () => {
+
+    // const categories = ['One Punch', 'Samurai X', 'Dragon Ball Z'];
+    const [categories, setcategories] = useState(['Dragon Ball Z']);
+
+    // const handleAdd = () => {
+
+    //     setcategories([...categories, 'HunterxHunter']);
+    //     //setcategories( cats => [ ...cats, 'HunterxHunter ]);
+
+    // }
+
+    return ( 
+        <>
+            <h1>GifExpertApp</h1>
+            <AddCategory setcategories={ setcategories }/>
+            <hr />
+
+            {/* <button onClick={ handleAdd }>Agregar</button> */}
+
+            <ol>
+                {
+                    categories.map( category =>  
+                        <GifGrid 
+                            category={ category } 
+                            key={category}
+                        />
+                    )
+                }
+            </ol>
+        </>
+     );
+}
+ 
+export default GifExpertApp;
+
---
+++ src/components/AddCategory.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import propTypes from 'prop-types';
+
+export const AddCategory = ({ setcategories }) => {
+
+    const [inputValue, setInputValue] = useState('');
+
+    const handleInputChange = (e) => {
+        // console.log(e.target.value)
+        setInputValue( e.target.value );
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if( inputValue.trim().length > 0) {
+            setcategories( cats => [inputValue, ...cats]);
+            setInputValue('');
+        }
+
+
+    }
+
+    AddCategory.propTypes = {
+        setcategories: propTypes.func.isRequired 
+    }
+
+
+  return (
+    <form onSubmit={ handleSubmit }>
+        <input 
+            type='text' 
+            value={ inputValue }
+            onChange={ handleInputChange }
+        />
+    </form>
+  )
+}
